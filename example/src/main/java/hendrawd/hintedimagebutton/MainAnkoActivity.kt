01bb9b5c76b38955ac7343298f28fafc5f9b6406
hendrawd <hendraz_88@yahoo.co.id>
2018-11-06 18:01:21 +0700

    Enable anko DSL

---
+++ example/src/main/java/hendrawd/hintedimagebutton/MainAnkoActivity.kt
@@ -0,0 +1,30 @@
+package hendrawd.hintedimagebutton
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import hendrawd.library.customview.hintedImageButton
+import org.jetbrains.anko.*
+
+class MainAnkoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        linearLayout {
+            layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
+            gravity = Gravity.CENTER
+            orientation = LinearLayout.VERTICAL
+
+            hintedImageButton {
+                contentDescription = getString(R.string.hint)
+                imageResource = R.mipmap.ic_launcher
+            }.lparams(wrapContent, wrapContent)
+
+            textView(R.string.long_click_button_above).lparams(wrapContent, wrapContent) {
+                topMargin = dip(8)
+            }
+        }
+    }
+}
