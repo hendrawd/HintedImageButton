6bc224385186d55e4e25fd78229416e0a975bdb1
Hendra Wijaya Djiono <hendraz_88@yahoo.co.id>
2019-05-24 23:00:33 +0700

    Create custom attribute for hint duration

--- example/src/main/java/hendrawd/hintedimagebutton/MainAnkoActivity.kt
+++ example/src/main/java/hendrawd/hintedimagebutton/MainAnkoActivity.kt
@@ -1,10 +1,10 @@
 package hendrawd.hintedimagebutton
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import hendrawd.library.customview.hintedImageButton
 import org.jetbrains.anko.*
 
@@ -20,6 +20,8 @@ class MainAnkoActivity : AppCompatActivity() {
             hintedImageButton {
                 contentDescription = getString(R.string.hint)
                 imageResource = R.mipmap.ic_launcher
+                // optional set hint duration HintedImageButton.DURATION_SHORT or DURATION_LONG
+                // hintDuration = HintedImageButton.DURATION_LONG
             }.lparams(wrapContent, wrapContent)
 
             textView(R.string.long_click_button_above).lparams(wrapContent, wrapContent) {
--- library/src/main/java/hendrawd/library/customview/HintedImageButton.kt
+++ library/src/main/java/hendrawd/library/customview/HintedImageButton.kt
@@ -1,12 +1,12 @@
 package hendrawd.library.customview
 
 import android.content.Context
-import androidx.appcompat.widget.AppCompatImageButton
 import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import android.view.View.OnLongClickListener
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
 
 /**
  * Show the content description in a Toast when the user long presses it.
@@ -17,12 +17,36 @@ import android.widget.Toast
  * @author Callum Taylor, hendrawd
  * @see [http://callumtaylor.net](http://callumtaylor.net)
  */
-class HintedImageButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null)
-    : AppCompatImageButton(context, attrs), OnLongClickListener {
+class HintedImageButton @JvmOverloads constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+) : AppCompatImageButton(context, attrs, defStyleAttr), OnLongClickListener {
+
     private var mOnLongClickListener: OnLongClickListener? = null
+    var hintDuration = DURATION_SHORT
+
+    companion object {
+        val DURATION_SHORT = 0
+        val DURATION_LONG = 1
+    }
 
     init {
         super.setOnLongClickListener(this)
+        val attributes = context.obtainStyledAttributes(
+                attrs,
+                R.styleable.HintedImageButton,
+                defStyleAttr,
+                0
+        )
+        try {
+            hintDuration = attributes.getInteger(
+                    R.styleable.HintedImageButton_hintDuration,
+                    DURATION_SHORT
+            )
+        } finally {
+            attributes.recycle()
+        }
     }
 
     override fun setOnLongClickListener(onLongClickListener: OnLongClickListener?) {
@@ -51,12 +75,16 @@ class HintedImageButton @JvmOverloads constructor(context: Context, attrs: Attri
                 getLocationOnScreen(positions)
 
                 val xOffset = positions[0] - contentDescription.length / 6
-                val yOffset = positions[1] - 128
+                val yOffset = positions[1] - 128 // TODO change y position based on view height
 
                 Toast.makeText(
                         context,
                         contentDescription,
-                        Toast.LENGTH_SHORT
+                        if (hintDuration == DURATION_SHORT) {
+                            Toast.LENGTH_SHORT
+                        } else {
+                            Toast.LENGTH_LONG
+                        }
                 ).apply {
                     setGravity(Gravity.TOP or Gravity.LEFT, xOffset, yOffset)
                     show()
